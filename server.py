072171e03e1da83920b4f3fe52732e866837e901
Alden Choe <aldenchoe@Aldens-MacBook-Pro.local>
2022-05-19 11:21:32 -0700

    runs

--- server.py
+++ server.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return "Hello world!"
+def home():
+    return render_template('home.html')
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 
 if __name__ == '__main__':
